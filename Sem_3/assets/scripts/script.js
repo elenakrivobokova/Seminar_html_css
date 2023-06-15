9b8a8e187f00ded3c313ba7fb96f18289ac5cc9c
Elena K <lena-k861@yandex.ru>
2023-06-16 00:34:03 +0300

    end

--- Sem_3/assets/scripts/script.js
+++ Sem_3/assets/scripts/script.js
@@ -12,23 +12,24 @@ let count = 0;
 
 function clickHead() {
     let element = document.getElementById('a1');
-    if (count % 2 == 0) element.classList.add('active'), element.textContent = "Это МЕЙН-КУН!";
-    else element.classList.remove('active'), element.textContent = "Что за зверь?"
-    
+    if (count % 2 == 0) {element.classList.add('active'), element.textContent = "Это МЕЙН-КУН!";}
+    else {element.classList.remove('active'), element.textContent = "Что за зверь?"}
+
     // count % 2 == 0 ? element.classList.add('active') : element.classList.remove('active');
     count++;
     // element.classList.contains('active');
     // element.classList.toggle('active');
     // element.textContent = "Это МЕЙН-КУН!";
-}let btn = document.getElementById('btn');
+}
+let btn = document.getElementById('btn');
 console.log(btn);
 btn.addEventListener('click', clickHead);
 
 
 function clickText() {
     let element = document.getElementById('a2');
-    if (count % 2 == 0) element.classList.add('description'), element.textContent = "Описание породы";
-    else element.classList.remove('description'), element.textContent = 'Одной из самых удивительных и загадочных пород считается мейн-кун – ласковый гигант с серьезным взглядом. Этих созданий называют «комнатными рысями», что неудивительно, т.к. они одни из самых крупных домашних кошек.'
+    if (count % 2 == 0) {element.classList.add('description'), element.textContent = "Описание породы";}
+    else {element.classList.remove('description'), element.textContent = 'Одной из самых удивительных и загадочных пород считается мейн-кун – ласковый гигант с серьезным взглядом. Этих созданий называют «комнатными рысями», что неудивительно, т.к. они одни из самых крупных домашних кошек.'}
     count++;
 }
 let btn2 = document.getElementsByClassName('ice');
